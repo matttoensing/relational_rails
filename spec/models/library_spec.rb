57edd0f5fffd596e930a9d4d213397c92c46532e
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-05 19:46:42 -0600

    Add validation tests

--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Library do
   describe 'validations' do
-
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:public)}
+    it {should validate_presence_of(:zip_code)}
   end
 
   describe 'associations' do
