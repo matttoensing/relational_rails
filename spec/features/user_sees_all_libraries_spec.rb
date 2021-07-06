d9f1fa37dc6ccc40fa868ecb7a28e759e57cd546
Brett Jones <bhjones4545@gmail.com>
2021-07-06 15:31:03 -0600

    Create tests for libraries create and see

--- spec/features/user_sees_all_libraries_spec.rb
+++ spec/features/user_sees_all_libraries_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe 'user sees all libraries' do
     it 'displays all libraries' do
       library1= Library.create!(name: 'Denver Public Library', public: true, zip_code: 12345)
       visit '/libraries'
+      expect(page).to have_content(library1.name)
+      expect(page).to have_content(library1.public)
+      expect(page).to have_content(library1.zip_code)
     end
   end
 end
