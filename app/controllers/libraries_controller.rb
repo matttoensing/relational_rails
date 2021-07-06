f61cb2be726bcb6d3d5c7dacd59bfdb5e01e502a
Brett Jones <bhjones4545@gmail.com>
2021-07-06 15:31:45 -0600

    Libraries index, new views with routes and controllers

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -2,4 +2,15 @@ class LibrariesController < ApplicationController
   def index
     @libraries = Library.all
   end
+
+  def new
+    @library = Library.new
+  end
+
+  def create
+    library = Library.new(name: params[:library][:name], public: params[:library][:public], zip_code: params[:library][:zip_code])
+    library.save
+
+    redirect_to '/libraries'
+  end
 end
