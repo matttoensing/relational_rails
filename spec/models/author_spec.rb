8cc6250ff385a6041b8d1748620271270a450277
Matt Toensing <matthew.toensing@gmail.com>
2021-07-08 13:44:42 -0700

    Include test for sorting records method

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe Author do
   describe 'associations' do
     it {should have_many :books}
   end
+
+  describe 'class methods' do
+    describe '#order_by_creation_time' do
+      it 'orders authors by creation time' do
+        Author.destroy_all
+        Book.destroy_all
+
+        author1 = Author.create!(name: 'Hunter S Thompson', published: true, age: 63, created_at: 10.seconds.ago)
+        author2 = Author.create!(name: 'Malcom Gladwell', published: true, age: 57, created_at: 36.seconds.ago)
+        author3 = Author.create!(name: 'Michael Lewis', published: true, age: 48, created_at: 48.seconds.ago)
+
+        expect(Author.order_by_creation_time.to_a).to eq([author1, author2, author3])
+      end
+    end
+  end
 end
