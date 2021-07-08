bd572b9398526ee7604e4fff3750c51bc8c34471
Brett Jones <bhjones4545@gmail.com>
2021-07-07 19:20:03 -0600

    WIP for sort by created at

--- spec/features/libraries/user_sees_all_libraries_spec.rb
+++ spec/features/libraries/user_sees_all_libraries_spec.rb
@@ -9,5 +9,30 @@ RSpec.describe 'user sees all libraries' do
       expect(page).to have_content(library1.public)
       expect(page).to have_content(library1.zip_code)
     end
+
+    it 'user sees a link that takes them to member index' do
+      library1= Library.create!(name: 'Denver Public Library', public: true, zip_code: 12345)
+
+      visit '/libraries'
+
+      click_link 'member index'
+
+      expect(page).to have_content()#have_length
+    end
+
+    # describe '#recently_created' do
+    #   library1 = Library.create!(name: 'Fort Collins Public Library', public: true, zip_code: 21543)
+    #   library2 = Library.create!(name: 'Denver Public Library', public: true, zip_code: 12345)
+    #
+    #   let(:this) {library1}
+    #   let(:that) {library2}
+    #
+    #   it 'will order by recently created' do
+    #
+    #     visit '/libraries'
+    #
+    #     expect(this).to appear_before(that)
+    #   end
+    # end
   end
 end
