369c2147a2b032dc793151f60deee0341d65db65
Matt Toensing <matthew.toensing@gmail.com>
2021-07-08 11:49:44 -0700

    Add class method to sort by created by attribute

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,4 +1,8 @@
 class Library < ApplicationRecord
   validates_presence_of :name, :public, :zip_code
   has_many :members
+
+  def self.order_by_creation_time
+    order('created_at DESC')
+  end
 end
--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -11,16 +11,36 @@ RSpec.describe Library do
     it {should have_many :members}
   end
 
-  # describe 'instance methods' do
-  #   it 'can sort our library index page by recently created' do
-  #     library1 = Library.create!(name: 'Fort Collins Public Library', public: true, zip_code: 21543)
-  #     library2 = Library.create!(name: 'Denver Public Library', public: true, zip_code: 12345)
-  #     library3 = Library.create!(name: 'Boise Public Library', public: true, zip_code: 54321)
-  #
-  #     expect(parent_sort).to eq(expected)
-  #   end
-  # end
+  describe 'class methods' do
+    describe '#order_by_creation_time' do
+      it 'orders libraries by creation time' do
+        Library.destroy_all
+        Member.destroy_all
+
+        library1 = Library.create!(name: 'Denver Public Library', public: true, zip_code: 12345, created_at: 10.seconds.ago)
+        library2 = Library.create!(name: 'Boise Public Library', public: true, zip_code: 54321, created_at: 46.seconds.ago)
+        library3 = Library.create!(name: 'Boulder Public Library', public: true, zip_code: 73613, created_at: 112.seconds.ago)
+
+        expect(Library.order_by_creation_time.to_a).to eq([library1, library2, library3])
+      end
+    end
+  end
 end
+
+
+
+
+
+
+# describe 'instance methods' do
+#   it 'can sort our library index page by recently created' do
+#     library1 = Library.create!(name: 'Fort Collins Public Library', public: true, zip_code: 21543)
+#     library2 = Library.create!(name: 'Denver Public Library', public: true, zip_code: 12345)
+#     library3 = Library.create!(name: 'Boise Public Library', public: true, zip_code: 54321)
+#
+#     expect(parent_sort).to eq(expected)
+#   end
+# end
 
 # describe '#recently_created' do
 #   library1 = Library.create!(name: 'Fort Collins Public Library', public: true, zip_code: 21543)
