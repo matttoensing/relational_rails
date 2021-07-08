d9ad35f63e4d4af0fa3a2d52d9f9bcb3f79b61ab
Matt Toensing <matthew.toensing@gmail.com>
2021-07-08 14:04:16 -0700

    Include order by created at method

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -1,4 +1,8 @@
 class Museum < ApplicationRecord
   validates_presence_of :name, :free, :entry_fee
   has_many :exhibits
+
+  def self.order_by_creation_time
+    order('created_at DESC')
+  end
 end
