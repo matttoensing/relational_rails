750e28d78af0fb2970a3a89bc4e42d5338bc8f4b
Matt Toensing <matthew.toensing@gmail.com>
2021-07-08 14:25:31 -0700

    Change member created at

--- spec/features/libraries/members/user_sees_all_members_spec.rb
+++ spec/features/libraries/members/user_sees_all_members_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'libraries member index' do
   it 'shows all the members of the library' do
     library = Library.create!(name: "Memphis Public Library", public: true,zip_code: 12345)
-    member1 = Member.create!(first_name: "Brett",last_name: "Jones", age: 27, late_fees: true, library_id: library.id)
-    member2 = Member.create!(first_name: "Matt",last_name: "Toensing", age: 33, late_fees: true, library_id: library.id)
+    member1 = library.members.create!(first_name: "Brett",last_name: "Jones", age: 27, late_fees: true)
+    member2 = library.members.create!(first_name: "Matt",last_name: "Toensing", age: 33, late_fees: true)
 
     visit "/libraries/#{library.id}/members"
 
