281647860760e7549360a774ae2331ccd13b931f
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-09 17:57:10 -0600

    Add new, create private methods

--- app/controllers/library_members_controller.rb
+++ app/controllers/library_members_controller.rb
@@ -1,6 +1,26 @@
 class LibraryMembersController < ApplicationController
   def index
-    library = Library.find(params[:library_id])
-    @members = library.members
+    @library = Library.find(params[:library_id])
+    @members = @library.members
+  end
+
+  def new
+    @library = Library.find(params[:library_id])
+    @member = Member.new
+  end
+
+  def create
+    @library = Library.find(params[:library_id])
+    member = Member.new(member_params)
+
+    member.save
+
+    redirect_to "/libraries/#{@library.id}/members"
+  end
+
+  private
+
+  def member_params
+    params.permit(:first_name, :last_name, :age, :late_fees, :library_id)
   end
 end
