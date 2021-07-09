84dcfbf298fcc7d0c7073a46d8116764a6158896
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-09 17:57:07 -0600

    Add new, create private methods

--- app/controllers/author_books_controller.rb
+++ app/controllers/author_books_controller.rb
@@ -1,6 +1,26 @@
 class AuthorBooksController < ApplicationController
   def index
-    author = Author.find(params[:author_id])
-    @books = author.books
+    @author = Author.find(params[:author_id])
+    @books = @author.books
+  end
+
+  def new
+    @author = Author.find(params[:author_id])
+    @book = Book.new
+  end
+
+  def create
+    @author = Author.find(params[:author_id])
+    book = Book.new(book_params)
+
+    book.save
+
+    redirect_to "/authors/#{@author.id}/books"
+  end
+
+  private
+
+  def book_params
+    params.permit(:title, :pages, :awards, :author_id)
   end
 end
