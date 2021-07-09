4f2297047310a808829e9fa0cc2e843815304578
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-09 17:57:13 -0600

    Add new, create private methods

--- app/controllers/museum_exhibits_controller.rb
+++ app/controllers/museum_exhibits_controller.rb
@@ -1,6 +1,26 @@
 class MuseumExhibitsController < ApplicationController
   def index
-    museum = Museum.find(params[:museum_id])
-    @exhibits = museum.exhibits
+    @museum = Museum.find(params[:museum_id])
+    @exhibits = @museum.exhibits
+  end
+
+  def new
+    @museum = Museum.find(params[:museum_id])
+    @exhibits = Exhibit.new
+  end
+
+  def create
+    @museum = Museum.find(params[:museum_id])
+    exhibit = Exhibit.new(exhibit_params)
+
+    exhibit.save
+
+    redirect_to "/museums/#{@museum.id}/exhibits"
+  end
+
+  private
+
+  def exhibit_params
+    params.permit(:title, :person_limit, :photos, :flash, :museum_id)
   end
 end
