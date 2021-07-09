9ee84afef3a38cef5322efd54c6e6f8bbc1ab1b5
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-08 18:14:48 -0600

    Add new/create methods for the museums controller

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -6,4 +6,16 @@ class MuseumsController < ApplicationController
   def show
     @museum = Museum.find(params[:id])
   end
+
+  def new
+    @museum = Museum.new
+  end
+
+  def create
+    museum = Museum.new(name: params[:museum][:name], free: params[:museum][:free], entry_fee: params[:museum][:entry_fee])
+
+    museum.save
+
+    redirect_to '/museums'
+  end
 end
