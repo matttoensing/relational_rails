f757688d0bf104081e2b628122df172b829218d0
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-08 18:14:41 -0600

    Add new/create methods for the authors controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,3 @@
-
 class AuthorsController < ApplicationController
   def index
     @authors = Author.all.order_by_creation_time
@@ -6,5 +5,17 @@ class AuthorsController < ApplicationController
 
   def show
     @author = Author.find(params[:id])
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    author = Author.new(name: params[:author][:name], published: params[:author][:published], age: params[:author][:age])
+
+    author.save
+
+    redirect_to '/authors'
   end
 end
