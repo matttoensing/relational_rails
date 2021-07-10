1a057d87705cc5e4b3d8fe80ffc4905e6a7c6ba8
Brett Jones <bhjones4545@gmail.com>
2021-07-09 19:54:47 -0600

    Create tests for edit books

---
+++ spec/features/books/edit_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'user can edit books on the author page' do
+  it 'shows edit page' do
+    author = Author.create!(name: "Ezze Alwafai", published: true, age: 35)
+    book = author.books.create!(title: "Game of Drones", pages: 500, awards: true)
+
+    visit "/books/#{book.id}"
+
+    click_on "Edit Book"
+
+    expect(current_path).to eq("/books/#{book.id}/edit")
+  end
+
+  it 'can edit the books' do
+    author = Author.create!(name: "Ezze Alwafai", published: true, age: 35)
+    book = author.books.create!(title: "Game of Drones", pages: 500, awards: true)
+
+    visit "/books/#{book.id}/edit"
+
+    fill_in "Title", with: "Matt Jones"
+    fill_in "Pages", with: '999'
+    fill_in "Awards", with: true
+save_and_open_page
+    click_on "Update Book"
+
+    expect(current_path).to eq("/books/#{book.id}")
+    expect(page).to have_content("Matt Jones")
+  end
+end
