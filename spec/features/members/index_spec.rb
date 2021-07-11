690da496ebc90ada30c0b939681490d31a2b8bc1
Brett Jones <bhjones4545@gmail.com>
2021-07-11 15:37:54 -0600

    Create members delete tests

--- spec/features/members/index_spec.rb
+++ spec/features/members/index_spec.rb
@@ -29,4 +29,35 @@ RSpec.describe 'Members index page' do
 
     expect(current_path).to eq("/members/#{member1.id}/edit")
   end
+
+  it 'can view and interact with details link' do
+    library = Library.create!(name: "Memphis Public Library", public: true,zip_code: 12345)
+    member1 = library.members.create!(first_name: "Brett",last_name: "Jones", age: 27, late_fees: true)
+    member2 = library.members.create!(first_name: "Matt",last_name: "Toensing", age: 33, late_fees: true)
+
+    visit "/members"
+
+    expect(page).to have_link("Edit: #{member1.first_name}")
+    expect(page).to have_link("Edit: #{member2.first_name}")
+
+    click_link "View: #{member1.first_name}"
+
+    expect(current_path).to eq("/members/#{member1.id}")
+    expect(page).to have_content(member1.first_name)
+  end
+
+  it 'can view and interact with delete link' do
+    library = Library.create!(name: "Memphis Public Library", public: true,zip_code: 12345)
+    member = library.members.create!(first_name: "Brett",last_name: "Jones", age: 27, late_fees: true)
+
+    visit "/members"
+
+    expect(current_path).to eq("/members")
+    expect(page).to have_content("Brett")
+
+    click_link "Delete: #{member.first_name}"
+
+    expect(current_path).to eq("/members")
+    expect(page).to_not have_content("Brett")
+  end
 end
--- spec/features/members/show_spec.rb
+++ spec/features/members/show_spec.rb
@@ -12,4 +12,28 @@ RSpec.describe 'member show page' do
     expect(page).to have_content(member.age)
     expect(page).to have_content("Late Fees: #{member.late_fees}")
   end
+
+  describe 'delete button' do
+    it 'can find delete button/link' do
+      library = Library.create!(name: "Denver Library", public: true, zip_code: 12345)
+      member = library.members.create!(first_name: "Ezze", last_name: "Alwafai", age:35 ,late_fees: true)
+
+      visit "/members/#{member.id}"
+
+      expect(page).to have_content("Delete: #{member.first_name}")
+    end
+
+    it 'can use the delete feature' do
+      library = Library.create!(name: "Denver Library", public: true, zip_code: 12345)
+      member = library.members.create!(first_name: "Ezze", last_name: "Alwafai", age:35 ,late_fees: true)
+
+      visit "/members/#{member.id}"
+
+      click_link "Delete: #{member.first_name}"
+
+      expect(current_path).to eq("/members")
+      expect(page).to_not have_content("Ezze")
+      expect(page).to_not have_content("Alwafai")
+    end
+  end
 end
