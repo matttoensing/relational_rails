4f8af1fd6cbc24bd6bdcd58ace277ecf63180f99
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-11 17:12:14 -0600

    Update method because it wasn't working

--- app/controllers/museum_exhibits_controller.rb
+++ app/controllers/museum_exhibits_controller.rb
@@ -7,6 +7,10 @@ class MuseumExhibitsController < ApplicationController
       @museum = Museum.find(params[:museum_id])
       @exhibits = @museum.exhibits
     end
+    if params.has_key?(:filter)
+      @museum = Museum.find(params[:museum_id])
+      @exhibits = @museum.exhibits.filter_over_person_limit(params[:filter].to_i)
+    end
   end
 
   def new
