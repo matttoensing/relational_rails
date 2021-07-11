9f5b169200e3690f006d401074d586334dcd7e02
Matt Toensing <matthew.toensing@gmail.com>
2021-07-11 09:04:29 -0700

    Include tests for delete link on index page and to delete record

--- spec/features/books/delete_spec.rb
+++ spec/features/books/delete_spec.rb
@@ -20,4 +20,24 @@ RSpec.describe "delete books" do
     expect(current_path).to eq("/books")
     expect(page).to_not have_content("Game of Drones")
   end
+
+  it 'user can see a button to delete a book from the show page' do
+    author = Author.create!(name: "Ezze Alwafai", published: true, age: 35)
+    book = author.books.create!(title: "Game of Drones", pages: 500, awards: true)
+
+    visit '/books'
+
+    expect(page).to have_button("Delete: #{book.title}")
+  end
+
+  it 'can delete a book record from the index page' do
+    author = Author.create!(name: "Ezze Alwafai", published: true, age: 35)
+    book = author.books.create!(title: "Game of Drones", pages: 500, awards: true)
+
+    visit "/books"
+    click_button "Delete: #{book.title}"
+
+    expect(current_path).to eq("/books")
+    expect(page).to_not have_content("Game of Drones")
+  end
 end
