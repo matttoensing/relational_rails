4368d2d37be4ad89c0c097e131862c2e58db2861
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-07-13 18:11:41 -0600

    Add extensions

--- app/models/author.rb
+++ app/models/author.rb
@@ -8,4 +8,8 @@ class Author < ApplicationRecord
   def number_of_books
     self.books.count
   end
+
+  def self.order_by_number_of_books
+    Author.select('authors.id, COUNT(books.id) AS books_count').joins(:books).group('authors.id').order('books_count DESC')
+  end
 end
--- spec/features/authors/index_spec.rb
+++ spec/features/authors/index_spec.rb
@@ -51,4 +51,27 @@ RSpec.describe 'user sees all authors' do
       expect(current_path).to eq("/authors/#{author1.id}/edit")
     end
   end
+
+  describe 'user can sort authors by the number of books' do
+    it 'user can see/click a link to sort authors' do
+      author1 = Author.create!(name: 'Hunter S Thompson', published: true, age: 63)
+      author2 = Author.create!(name: 'Malcom Gladwell', published: true, age: 57)
+      author3 = Author.create!(name: 'Michael Lewis', published: true, age: 48)
+      book1 = author2.books.create!(title: "Game of Rails", pages: 400, awards: true)
+      book2 = author2.books.create!(title: "Ruby on Thrones", pages: 562, awards: true)
+      book3 = author2.books.create!(title: "Havasu Tour Guide", pages: 125, awards: true)
+      book4 = author3.books.create!(title: "Game of Fails", pages: 400, awards: true)
+      book5 = author3.books.create!(title: "Ruby on Ales", pages: 562, awards: true)
+      book6 = author1.books.create!(title: "Grand Canyon Tour Guide", pages: 125, awards: true)
+
+      visit '/authors'
+
+      expect(page).to have_link("Sort Authors by Number of Books")
+
+      click_link "Sort Authors by Number of Books"
+
+      expect(author2.name).to appear_before(author3.name)
+      expect(author3.name).to appear_before(author1.name)
+    end
+   end
 end
